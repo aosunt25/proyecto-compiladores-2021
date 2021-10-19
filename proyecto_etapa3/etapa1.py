95013acd831fce93c0661ba3635f7570cacc7ad2
Fabricio <fabricio.fuentes12@gmail.com>
2021-10-19 17:15:30 -0500

    Etapa 1 completada

--- proyecto_etapa3/etapa1.py
+++ proyecto_etapa3/etapa1.py
@@ -9,11 +9,47 @@ from myexceptions import *
 
 
 class Listener(CoolListener):
+    # No Main
     tiene_main = False
 
     def enterKlass(self, ctx: CoolParser.KlassContext):
         if ctx.TYPE(0).getText() == "Main":
             self.tiene_main = True
+
+        if ctx.TYPE(1) != None and (
+            ctx.TYPE(1).getText() == "Bool"
+            or ctx.TYPE(1).getText() == "String"
+            or ctx.TYPE(1).getText() == "SELF_TYPE"
+        ):
+            raise InvalidInheritsException
+
+        if ctx.TYPE(0).getText() == "Object":
+            raise RedefineBasicClassException
+
+        if ctx.TYPE(0).getText() == "Int":
+            raise RedefineBasicClassException
+
+        if ctx.TYPE(0).getText() == "SELF_TYPE":
+            raise RedefineBasicClassException
+
+    def enterFormal(self, ctx: CoolParser.FormalContext):
+        if ctx.ID().getText() == "self":
+            raise SelfVariableException
+
+        if ctx.TYPE().getText() == "SELF_TYPE":
+            raise SelftypeInvalidUseException
+
+    def enterAtribute(self, ctx: CoolParser.AtributeContext):
+        if ctx.ID().getText() == "self":
+            raise SelfVariableException
+
+    def enterLet(self, ctx: CoolParser.LetContext):
+        if ctx.ID(0).getText() == "self":
+            raise SelfVariableException
+
+    def enterAssign(self, ctx: CoolParser.AssignContext):
+        if ctx.ID().getText() == "self":
+            raise SelfAssignmentException
 
     def exitProgram(self, ctx: CoolParser.ProgramContext):
         if not self.tiene_main:
@@ -23,7 +59,7 @@ class Listener(CoolListener):
 def parseCase(caseName):
     parser = CoolParser(
         CommonTokenStream(
-            CoolLexer(FileStream("../resources/semantic/input/%s.cool" % caseName))
+            CoolLexer(FileStream("./resources/semantic/input/%s.cool" % caseName))
         )
     )
     return parser.program()
@@ -36,7 +72,7 @@ class CoolTests(unittest.TestCase):
     def test1(self):
         tree = parseCase("nomain")
         with self.assertRaises(NoMainException):
-            print(self.walker.walk(CoolListener, tree))
+            print(self.walker.walk(Listener(), tree))
 
     def test2(self):
         tree = parseCase("badredefineint")
